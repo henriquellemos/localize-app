5281b294f88eeef1650e2eed5c36664937633141
victor souza marques <32906289+victorsouzamarques@users.noreply.github.com>
2019-11-21 17:35:17 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
-//Install express server
 const express = require('express');
 const path = require('path');
-
+const nomeApp = process.env.npm_package_name;
 const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/localize-app'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/localize-app/index.html'));
+app.use(express.static(`${__dirname}/dist/${nomeApp}`));
+app.get('/*', (req, res) => {
+res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
 });
-
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
