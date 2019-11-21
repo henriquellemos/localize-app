826e58030527b64100b29ca0026d82dcb123e965
Henrique Lopes Lemos <henrique.lemos@verity.local>
2019-11-21 17:57:28 -0300

    Login implementado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,9 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  submitted = false
+  credencialsCorrect = true;
+  user = {
+    email:'henrique@ufu.br',
+    senha:'123123'
   }
 
+  user1 = {
+    email:'larissa@ufu.br',
+    senha:'123123'
+  }
+
+  user2 = {
+    email:'israel@ufu.br',
+    senha:'123123'
+  }
+
+  loginForm: FormGroup;
+  constructor(private formBuilder: FormBuilder, public router: Router) { }
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required]],
+      senha: ['', [Validators.required, Validators.minLength(2)]]
+    });
+  }
+  // convenience getter for easy access to form fields
+  get f() { return this.loginForm.controls; }
+  onSubmit() {
+      // stop here if form is invalid
+      if (this.loginForm.invalid) {
+        return;
+      }
+      if (this.user.email == this.loginForm.value.email && this.user.senha == this.loginForm.value.senha){
+        this.router.navigate(['/home'])
+      }
+      if (this.user1.email == this.loginForm.value.email && this.user1.senha == this.loginForm.value.senha){
+        this.router.navigate(['/home'])
+      }
+      if (this.user2.email == this.loginForm.value.email && this.user2.senha == this.loginForm.value.senha){
+        this.router.navigate(['/home'])
+      }
+      
+      this.credencialsCorrect = false
+  }
 }
